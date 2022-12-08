1584435df857ff7d941acdf1b28c38a77391e41c
Bhupen Pal <bhupen@tealfeed.com>
2022-12-08 09:28:49 +0530

    fix: webpack error

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,6 @@ import { PrismaModule } from './prisma/prisma.module';
 import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [ConfigModule, UserModule, AuthModule, PrismaModule, RedisModule],
+  imports: [ConfigModule, RedisModule, UserModule, AuthModule, PrismaModule],
 })
 export class AppModule {}
